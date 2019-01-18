c5d6e6fe1fa52945effc7afdffc44938346b42a8
Анатолий Кондратов <a.kondratov@bftcom.com>
2019-01-18 18:39:58 +0300

    End part 28

---
+++ web-customer-tracker/src/com/luv2code/testdb/TestDbServlet.java
@@ -0,0 +1,15 @@
+package com.luv2code.testdb;
+
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@WebServlet("/TestDbServlet")
+public class TestDbServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
